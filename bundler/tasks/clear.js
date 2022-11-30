75d045e4635fc45e12fa71d1936b093d0bb35223
Nikita Shevchenko <nikita_shevchenko@inbox.ru>
2022-11-30 15:34:58 +0400

    fix: gulp::deleteFiles - Исправлен шаблон для "deleteSync"

--- bundler/tasks/clear.js
+++ bundler/tasks/clear.js
@@ -1,8 +1,15 @@
-import { buildFolder } from '../settings.js';
+import { _serve, _build } from '../settings.js';
 import { deleteSync } from 'del';
 
 const deleteFiles = (cb) => {
-   deleteSync([`${buildFolder}/**/*`, `!${buildFolder}/fonts`, `!${buildFolder}/fonts/**/*.*`]);
+   const status = process.env.NODE_ENV;
+   const folder = status === 'development' ? _serve : _build;
+   const patterns = {
+      development: [`${folder}/**/*`, `!${folder}/fonts`, `!${folder}/fonts/**/*.*`],
+      production: [`${folder}/**/*`]
+   };
+
+   deleteSync(patterns[status]);
    cb();
 };
 
