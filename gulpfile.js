f07247f6a99bc558635d2058cc0b920404e060d3
Nikita Shevchenko <nikita_shevchenko@inbox.ru>
2022-12-01 18:36:36 +0400

    fix: gulp::css.js - renamed

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import startServer from './bundler/tasks/server.js';
 import deleteFiles from './bundler/tasks/clear.js';
 import configureHtmlFiles from './bundler/tasks/html.js';
-import styles from './bundler/tasks/css.js';
+import styles from './bundler/tasks/Styles.js';
 import js from './bundler/tasks/JavaScript.js';
 import images from './bundler/tasks/Images.js';
 import { convertTtf2Woff, convertTtf2Woff2, setFontFaceMixin } from './bundler/tasks/Fonts.js';
