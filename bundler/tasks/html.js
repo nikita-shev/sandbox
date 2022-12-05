8e0f5869e239850fc6707b833891362ff956d9d0
Nikita Shevchenko <nikita_shevchenko@inbox.ru>
2022-12-05 10:33:01 +0400

    feat: gulp::html - Добавлен "w3c-html-validator"

--- bundler/tasks/html.js
+++ bundler/tasks/html.js
@@ -2,6 +2,7 @@ import { path } from '../settings.js';
 
 import gulp from 'gulp';
 import fileInclude from 'gulp-file-include';
+import { htmlValidator } from 'gulp-w3c-html-validator';
 import browserSync from 'browser-sync';
 
 const { src, dest } = gulp;
@@ -10,7 +11,12 @@ const { html } = path;
 const configureHtmlFiles = () => {
    const folder = process.env.NODE_ENV === 'development' ? html.public : html.build;
 
-   return src(html.src).pipe(fileInclude()).pipe(dest(folder)).pipe(browserSync.stream());
+   return src(html.src)
+      .pipe(fileInclude())
+      .pipe(htmlValidator.analyzer())
+      .pipe(htmlValidator.reporter())
+      .pipe(dest(folder))
+      .pipe(browserSync.stream());
 };
 
 export default configureHtmlFiles;
