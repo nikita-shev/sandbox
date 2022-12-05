a6dbe30b2d35a602e4b94db07ac70e0b458587a5
Nikita Shevchenko <nikita_shevchenko@inbox.ru>
2022-12-05 12:14:41 +0400

    fix: gulp::html - Исправлена build-версия

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 import gulp from 'gulp';
 import startServer from './bundler/tasks/server.js';
 import deleteFiles from './bundler/tasks/clear.js';
-import configureHtmlFiles from './bundler/tasks/html.js';
+import html from './bundler/tasks/HTML.js';
 import styles from './bundler/tasks/Styles.js';
 import js from './bundler/tasks/JavaScript.js';
 import images from './bundler/tasks/Images.js';
 import { convertTtf2Woff, convertTtf2Woff2, setFontFaceMixin } from './bundler/tasks/Fonts.js';
 
 const { series, parallel } = gulp;
+const { configureHtmlFiles } = html;
 const { configureStyleFiles } = styles;
 const { configureJsFiles } = js;
 const { convertToWebp, optimizeImages, createSvgSprite } = images;
