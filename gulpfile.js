713867ee4d22274337db4e2301d1de9817a37135
Nikita Shevchenko <nikita_shevchenko@inbox.ru>
2022-12-07 19:11:43 +0400

    feat: gulp::gulpfile.js - Добавлен "наблюдатель"

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+import { path } from './bundler/settings.js';
+
 import gulp from 'gulp';
 import startServer from './bundler/tasks/server.js';
 import deleteFiles from './bundler/tasks/clear.js';
@@ -7,11 +9,21 @@ import js from './bundler/tasks/JavaScript.js';
 import images from './bundler/tasks/Images.js';
 import { convertTtf2Woff, convertTtf2Woff2, setFontFaceMixin } from './bundler/tasks/Fonts.js';
 
-const { series, parallel } = gulp;
+const { html: HTML, css: CSS, js: JS, fonts: FONTS, images: IMAGES, svg: SVG } = path;
+const { series, parallel, watch } = gulp;
 const { configureHtmlFiles } = html;
 const { configureStyleFiles } = styles;
 const { configureJsFiles } = js;
 const { convertToWebp, optimizeImages, createSvgSprite } = images;
+
+const watchFiles = () => {
+   watch(HTML.watch, configureHtmlFiles);
+   watch(CSS.watch, configureStyleFiles);
+   watch(JS.watch, configureJsFiles);
+   watch(FONTS.watch, parallel(convertTtf2Woff, convertTtf2Woff2, setFontFaceMixin));
+   watch(IMAGES.watch, parallel(convertToWebp, optimizeImages));
+   watch(SVG.watch, createSvgSprite);
+};
 
 export default series(
    deleteFiles,
@@ -26,5 +38,5 @@ export default series(
       convertTtf2Woff2,
       setFontFaceMixin
    ),
-   parallel(startServer)
+   parallel(watchFiles, startServer)
 );
